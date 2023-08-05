9e6fb8371e43631d092b502334739fdebff61690
suman <suman.rajput1507@gmail.com>
2023-08-05 11:29:04 +0530

    more pages add

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-$(document).ready(function(){
-    $("#hamburger").click(function(){
-      $(".navopen").addClass("open");
-      })
-      $("#close").click(function(){
-        $(".navopen").removeClass("open");
-      })
+$(document).ready(function () {
+  $("#hamburger").click(function () {
+    $(".navopen").addClass("open");
+  });
+  $("#close").click(function () {
+    $(".navopen").removeClass("open");
+  });
 });
-$(document).ready(function() {
-        $('.owl-carousel').owlCarousel({
-            autoPlay: 3000,
-            items : 1,
-            loop: true,
-            
-        });
-    });
 
-    const inputs = document.querySelectorAll('input');
-    const comment = document.querySelector('.form-comment');
-    const submit = document.querySelector('.form-submit');
-    
-    function init() {
-      const isFill = [...inputs,comment].every(({value}) => value.trim().length);
-      submit.classList.toggle('enable',isFill);
-    }
-    
-    document.addEventListener('keyup',init,false);
+const inputs = document.querySelectorAll("input");
+const comment = document.querySelector(".form-comment");
+const submit = document.querySelector(".form-submit");
 
-    $(window).scroll(function(){
-      if ($(this).scrollTop() >= 10) {
-         $('#dynamic').addClass('nav-fixed');
-      } else {
-         $('#dynamic').removeClass('nav-fixed');
-      }
-   });
+function init() {
+  const isFill = [...inputs, comment].every(({ value }) => value.trim().length);
+  submit.classList.toggle("enable", isFill);
+}
+
+document.addEventListener("keyup", init, false);
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 10) {
+    $("#dynamic").addClass("nav-fixed");
+  } else {
+    $("#dynamic").removeClass("nav-fixed");
+  }
+});
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 10) {
+    $("#details").addClass("fixed");
+  } else {
+    $("#details").removeClass("fixed");
+  }
+});
 
 
+$(document).ready(function () {
+  $(".top_link").click(function () {
+    $(".top_link").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".tab_link").click(function(){
+    $(".tab_link").removeClass("tabactive")
+    $(".content_box").removeClass("tab_show");
+    $(this).addClass("tabactive");
+    let tabValue= $(this).attr("id");
+     $(`.${tabValue}`).addClass("tab_show");
+  });
+});
 
